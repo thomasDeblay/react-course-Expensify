25d9132c37726626eb81089026ed0ce7fa472df9
TommyDeblay <deblay.thomas@gmail.com>
2017-11-21 20:21:51 +0100

    setup the remove items code

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -29,10 +29,19 @@ export const startAddExpense = (expenseData = {}) => {
 };
 
   // REMOVE_EXPENSE
-  export const removeExpense = ({ id } = {}) => ({
+  export const removeExpense = ({id} = {}) => ({
     type: 'REMOVE_EXPENSE',
     id
   });
+
+  export const startRemoveExpense = ({id} = {}) => {
+    return (dispatch) => {
+
+      database.ref(`expenses/${id}`).remove().then(() => {
+        dispatch(removeExpense({id}))
+      })
+    };
+  };
   
   // EDIT_EXPENSE
   export const editExpense = (id, updates) => ({
